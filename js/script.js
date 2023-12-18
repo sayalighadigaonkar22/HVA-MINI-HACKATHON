ac1910eccfe42ca341bcf111929deadcdb60c2f5
SAYALI <sayalighadigaonkar22@navgurukul.org>
2023-12-18 09:33:01 +0530

    new-api-project

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const goTab = document.getElementById('goSearch');
+const searchTab = document.getElementById('searchInput');
+const newsImages = document.querySelectorAll('.loadImages');
+const newsTitles = document.querySelectorAll('.title');
+const newsDescription = document.querySelectorAll('.description');
+const newUrl = document.getElementsByTagName('a');
+
+// const defaultUrl = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}`;
+const apiKey = "7bc36466cef54d26bbd835df2170fe9c";
+
+goTab.addEventListener("click",async()=>{
+    const inputValue = searchTab.value;
+    const url = `https://newsapi.org/v2/everything?q=${inputValue}&apiKey=${apiKey}`;
+    const newInfo = await fetch(url)
+    const newsData = await newInfo.json();
+    console.log(newsData);
+
+    let count = 0;
+    for(let i=0; i<newsData.articles.length; i++){
+        // console.log(newsData.articles[i]);
+        newsImages[i].src=newsData.articles[i].urlToImage;
+        newsTitles[i].innerHTML =newsData.articles[i].title;
+        newsDescription[i].innerHTML =newsData.articles[i].description;
+        newUrl[i].href =newsData.articles[i].url;
+        count++;
+        if(count===9){
+            break;
+        }
+    }
+})
+
+
+
