aedba53a6837e7f52d05e096db7a3f2e57db8364
SAYALI <sayalighadigaonkar22@navgurukul.org>
2024-01-25 10:12:08 +0530

    interview

--- js/script.js
+++ js/script.js
@@ -5,29 +5,55 @@ const newsTitles = document.querySelectorAll('.title');
 const newsDescription = document.querySelectorAll('.description');
 const newUrl = document.getElementsByTagName('a');
 
-// const defaultUrl = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}`;
 const apiKey = "7bc36466cef54d26bbd835df2170fe9c";
 
-goTab.addEventListener("click",async()=>{
+goTab.addEventListener("click", () => {
+    getData();
+});
+
+searchTab.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        getData();
+    }
+});
+
+async function getData() {
     const inputValue = searchTab.value;
     const url = `https://newsapi.org/v2/everything?q=${inputValue}&apiKey=${apiKey}`;
-    const newInfo = await fetch(url)
+    const newInfo = await fetch(url);
     const newsData = await newInfo.json();
     console.log(newsData);
 
     let count = 0;
-    for(let i=0; i<newsData.articles.length; i++){
-        // console.log(newsData.articles[i]);
-        newsImages[i].src=newsData.articles[i].urlToImage;
-        newsTitles[i].innerHTML =newsData.articles[i].title;
-        newsDescription[i].innerHTML =newsData.articles[i].description;
-        newUrl[i].href =newsData.articles[i].url;
+    for (let i = 0; i < newsData.articles.length; i++) {
+        newsImages[i].src = newsData.articles[i].urlToImage;
+        newsTitles[i].innerHTML = newsData.articles[i].title;
+        newsDescription[i].innerHTML = newsData.articles[i].description;
+        newUrl[i].href = newsData.articles[i].url;
         count++;
-        if(count===9){
+        if (count === 9) {
             break;
         }
     }
-})
+}
 
+    // const url = `https://newsapi.org/v2/everything?q=${inputValue}&apiKey=${apiKey}`;
+    // const newInfo = await fetch(url)
+    // const newsData = await newInfo.json();
+    // console.log(newsData);
+
+
+    // let count = 0;
+    // for(let i=0; i<newsData.articles.length; i++){
+    //     // console.log(newsData.articles[i]);
+    //     newsImages[i].src=newsData.articles[i].urlToImage;
+    //     newsTitles[i].innerHTML =newsData.articles[i].title;
+    //     newsDescription[i].innerHTML =newsData.articles[i].description;
+    //     newUrl[i].href =newsData.articles[i].url;
+    //     count++;
+    //     if(count===9){
+    //         break;
+    //     }
+    // }
 
 
